8e979786dbae55a3d0f3fedfa224799fc149f2e4
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2019-03-14 16:43:48 +0100

    modified:   core/postprocess.py 	modified:   main.py

--- core/postprocess.py
+++ core/postprocess.py
@@ -2,12 +2,16 @@ import socket, urllib, lxml.html
 from core.url import URL
 from core.locals import *
 
-def manipulate_urls(wf):
+def proxify_css(wf):
+    # TODO
+    return wf
+
+def proxify_html(wf): # proxify_html
     try:
         content = wf.get_decoded_content()
         tree = lxml.html.fromstring(content) # Parse whole html tree
 
-        for el in [["a", "href"], ["link", "href"], ["script", "src"], ["img", "src"]]: # Crawl elements with URL
+        for el in [["a", "href"], ["link", "href"], ["script", "src"], ["img", "src"], ["form", "action"]]: # Crawl elements with URL
             for link_element in tree.xpath('//' + el[0]):
                 url = link_element.get(el[1]) # Get original URL
                 if url:
@@ -16,8 +20,15 @@ def manipulate_urls(wf):
 
         wf.content = lxml.html.tostring(tree, encoding=wf.encoding)
     except:
-        raise
-        print("[WARN] Couldn't manipulate URLs. Page \"%s\" may be broken." % (url))
+        #raise
+        print("[WARN] Couldn't proxify URLs. Page \"%s\" may be broken." % (wf.url))
+    return wf
+
+def proxify(wf):
+    if wf.type == "text/html":
+        proxify_html(wf)
+    elif wf.type == "text/css":
+        proxify_css(wf)
     return wf
 
 def _to_cache_url(origin, link):
