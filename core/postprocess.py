dd0f248d4b72ab0f7ce08174ea3681ba437edfdc
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2019-03-14 19:05:57 +0100

    cache/ 	test.py

--- core/postprocess.py
+++ core/postprocess.py
@@ -2,11 +2,48 @@ import socket, urllib, lxml.html
 from core.url import URL
 from core.locals import *
 
+"""
+I borrowed this code piece (proxifyCSS) of joshdick's miniProxy.php
+Link: https://github.com/joshdick/miniProxy
+Thank you.
+"""
+# Proxify contents of url() references in blocks of CSS text.
 def proxify_css(wf):
-    # TODO
+    # Add a "url()" wrapper to any CSS @import rules that only specify a URL without the wrapper,
+    # so that they're proxified when searching for "url()" wrappers below.
+    content = wf.get_decoded_content()
+
+    source_lines = content.split("\n")
+    normalized_lines = []
+    for line in source_lines:
+        if re.match(r"@import\s+url", line):
+            normalized_lines.append(line)
+        else:
+            def repl(m):
+                return m.group(1) + "url(" + m.group(2) + ")" + m.group(3)
+
+            normalized_lines.append(re.sub(r"(@import\s+)([^;\s]+)([\s;])", repl, line))
+
+    normalized_css = "\n".join(normalized_lines)
+    wf_url = wf.url.resolve()
+    def repl2(m):
+        url = m.group(1)
+        # Remove any surrounding single or double quotes from the URL so it can be passed to rel2abs - the quotes are optional in CSS
+        # Assume that if there is a leading quote then there should be a trailing quote, so just use trim() to remove them
+        if url.find("'") == 0:
+            url = url.strip("'")
+        if url.find("\"") == 0:
+            url = url.strip("\"")
+
+        if url.find("data:") == 0:
+            return "url(" + url + ")" # The URL isn't an HTTP URL but is actual binary data. Don't proxify it.
+        return "url(" + _to_cache_url(wf_url, url) + ")"
+
+    wf.set_decoded_content(re.sub(r"url\((.*?)\)", repl2, normalized_css))
     return wf
 
-def proxify_html(wf): # proxify_html
+
+def proxify_html(wf):
     try:
         content = wf.get_decoded_content()
         tree = lxml.html.fromstring(content) # Parse whole html tree
